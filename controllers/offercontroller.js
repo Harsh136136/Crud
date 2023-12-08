499dab2470cd6bc4f9c3e38e28c609bce3e162e7
harsh <harshlathiya91@gmail.com>
2023-12-08 15:07:30 +0530

    first commit

---
+++ controllers/offercontroller.js
@@ -0,0 +1,14 @@
+const offermodel = require('../models/offermodel');
+const path = require('path');
+
+module.exports.insertofferData = async (req, res) => {
+    // console.log(req.body);
+ 
+            req.body.isActive = true;
+            req.body.currentDate = new Date().toLocaleString();
+            req.body.updateDate = new Date().toLocaleString();
+            const offerdata = await offermodel.create(req.body)
+    
+
+return res.redirect('back');
+}
